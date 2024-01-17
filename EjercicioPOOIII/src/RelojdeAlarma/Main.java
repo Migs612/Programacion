14ef5fb66cc7a11df1cf6d27c4763e564a4e3e96
Migs612 <gutierrezsivilamanuel@gmail.com>
2024-01-17 17:09:04 +0100

    Añadido el ejercicio de RelojdeAlarma

--- EjercicioPOOIII/src/RelojdeAlarma/Main.java
+++ EjercicioPOOIII/src/RelojdeAlarma/Main.java
@@ -1,4 +1,41 @@
 package RelojdeAlarma;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        RelojdeAlarma Casio = new RelojdeAlarma();
+
+        System.out.println("Hora actual :");
+        System.out.println(Casio.getHoraActual());
+
+        System.out.println("Quieres activar una alarma?");
+        String activar = lector.nextLine();
+        boolean alarmaActiva;
+
+        if (activar.equalsIgnoreCase("Si")) {
+            alarmaActiva = true;
+            Casio.setAlarmaActiva(  alarmaActiva);
+            System.out.println("Ingrese la hora de la alarma (formato HH:mm): ");
+            String horaAlarma = lector.nextLine();
+
+            String[] partes = horaAlarma.split(":");
+
+            if (partes.length == 2) {
+                try {
+                    int hora = Integer.parseInt(partes[0]);
+                    int minutos = Integer.parseInt(partes[1]);
+                    Casio.setHoraAlarma(hora, minutos);
+                } catch (NumberFormatException e) {
+                    System.out.println("Formato incorrecto. Ingrese la hora de la alarma en el formato correcto.");
+                }
+            } else {
+                System.out.println("Formato incorrecto. Ingrese la hora de la alarma en el formato correcto.");
+            }
+        } else if (activar.equalsIgnoreCase("No")) {
+            System.out.println("Esta bien, disfrute el dia");
+        }
+    }
 }
--- EjercicioPOOIII/src/RelojdeAlarma/RelojdeAlarma.java
+++ EjercicioPOOIII/src/RelojdeAlarma/RelojdeAlarma.java
@@ -1,4 +1,41 @@
 package RelojdeAlarma;
 
+import java.time.LocalDateTime;
+
 public class RelojdeAlarma {
+    LocalDateTime locaDate = LocalDateTime.now();
+    private int hora;
+    private int minutos;
+    private String horaAlarma;
+    private String horaActual;
+    private boolean alarmaActiva;
+
+    public RelojdeAlarma() {
+        hora = locaDate.getHour();
+        minutos = locaDate.getMinute();
+        horaAlarma = hora+":"+ minutos;
+        horaActual = hora+":"+ minutos;
+        alarmaActiva = false;
+    }
+
+    public String getHoraAlarma() {
+        return horaAlarma;
+    }
+    public void setHoraAlarma(int hora, int minutos) {
+        this.hora = hora;
+        this.minutos = minutos;
+        this.horaAlarma = hora + ":" + minutos;
+    }
+    public String getHoraActual() {
+        return horaActual;
+    }
+    public void setHoraActual() {
+        LocalDateTime nuevaFecha = LocalDateTime.now();
+        this.hora = nuevaFecha.getHour();
+        this.minutos = nuevaFecha.getMinute();
+        this.horaActual = hora + ":" + minutos;
+    }
+    public void setAlarmaActiva(boolean alarmaActiva) {
+        this.alarmaActiva = alarmaActiva;
+    }
 }
